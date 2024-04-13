310bdf6502f6f479d27515d927bb33ca6e66940e
SangWonLee <tkddnjs0687@naver.com>
2024-04-13 14:52:51 +0900

    chore: 프로젝트 환경설정

---
+++ src/pages/_app.tsx
@@ -0,0 +1,39 @@
+import { ReactElement, ReactNode } from "react";
+import { NextComponentType, NextPage } from "next";
+import type { AppContext, AppInitialProps, AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Global, ThemeProvider } from "@emotion/react";
+
+import { queryClient } from "@/services";
+import { globalStyles, theme } from "@/styles";
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const App: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
+  Component,
+  pageProps,
+}: AppPropsWithLayout) => {
+  const getLayout = Component.getLayout ?? ((page: ReactElement) => page);
+
+  return (
+    <>
+      <Global styles={globalStyles} />
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <ThemeProvider theme={theme}>
+            {getLayout(<Component {...pageProps} />)}
+          </ThemeProvider>
+        </RecoilRoot>
+      </QueryClientProvider>
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+
+const index = () => {
+  return <Wrapper>Login</Wrapper>;
+};
+
+export default index;
+
+const Wrapper = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.color.red10};
+  `}
+`;
