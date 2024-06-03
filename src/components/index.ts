f6845951f1337c5e58ceb239f7fd9221477e8ecf
SangWonLee <tkddnjs0687@naver.com>
2024-06-03 16:47:42 +0900

    feat: Tab, TabLayout 컴포넌트 제작

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from "./layout";
 export { default as Footer } from "./footer/Footer";
 export { default as Header } from "./header/Header";
+export { default as Tab } from "./tab/Tab";
---
+++ src/types/common.ts
@@ -0,0 +1,4 @@
+export interface SelectOptionType {
+  key: string;
+  label: string;
+}
