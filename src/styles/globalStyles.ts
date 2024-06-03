78ed1f5ef4ed23a6880f912b1918220fdf5844b8
SangWonLee <tkddnjs0687@naver.com>
2024-06-03 16:48:08 +0900

    feat: 글로벌 스타일 추가

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,8 +1,31 @@
 import { css } from "@emotion/react";
 
 const globalStyles = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html,
+  body {
+    height: 100%;
+  }
   html {
     font-size: 62.5%;
+  }
+  body {
+    overflow-x: hidden;
+    overflow-y: overlay;
   }
 
   a {
@@ -19,6 +42,16 @@ const globalStyles = css`
   ol,
   ul {
     list-style: none;
+  }
+
+  button {
+    border: 0;
+    background-color: transparent;
+    cursor: pointer;
+
+    &:disabled {
+      cursor: not-allowed;
+    }
   }
 `;
 
