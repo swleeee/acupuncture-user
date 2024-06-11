bc3310012efc3555a910ed0ea0d07c0fdbcf9977
SangWonLee <tkddnjs0687@naver.com>
2024-06-11 22:51:48 +0900

    chore: svg import 관련 svgr 라이브러리 설치 및 세팅

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
 
   webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
     if (!isServer) {
       config.resolve.fallback = {
         fs: false,
