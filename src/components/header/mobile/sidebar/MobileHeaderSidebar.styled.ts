28ff9e437b6d1ff643aaebbcae4f597e9ce2c17c
SangWonLee <tkddnjs0687@naver.com>
2024-06-15 23:11:24 +0900

    feat: 모바일 해상도 사이브다 스크린 제작

--- src/components/header/mobile/sidebar/MobileHeaderSidebar.styled.ts
+++ src/components/header/mobile/sidebar/MobileHeaderSidebar.styled.ts
@@ -1,5 +1,6 @@
+import Link from "next/link";
 import styled from "@emotion/styled";
-import { css, keyframes } from "@emotion/react";
+import { css, keyframes, Theme } from "@emotion/react";
 
 const openSidebar = keyframes` 
   0% { left: 100% }
@@ -20,5 +21,127 @@ export const Sidebar = styled.aside<{ isShow: boolean }>`
     z-index: ${theme.zIndex.SIDEBAR};
     animation: ${isShow ? openSidebar : closeSidebar} 0.5s forwards;
     transition: all 0.3s cubic-bezier(0.93, 0.635, 0.65, 0.97);
+  `}
+`;
+
+export const MenuWrapper = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 50px;
+    padding: 0 15px;
+    background-color: ${theme.color.primary};
+  `}
+`;
+
+const iconStyle = (theme: Theme) => css`
+  & > svg > g > path {
+    fill: ${theme.color.white};
+  }
+`;
+
+// TODO: SVG 수정
+export const CloseButton = styled.button`
+  ${({ theme }) => css`
+    ${iconStyle(theme)};
+  `}
+`;
+
+export const ProfileLink = styled(Link)`
+  ${({ theme }) => css`
+    ${iconStyle(theme)};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid ${theme.color.white};
+    border-radius: 50%;
+  `}
+`;
+
+export const MainNavItem = styled.li`
+  ${({ theme }) => css`
+    border-bottom: 1px solid ${theme.color.gray_200};
+  `}
+`;
+
+const open = (theme: Theme) => keyframes`
+  0% {
+    display: none;
+    height: 0px;
+    border-top: 0;
+    padding: 0 15px;
+    overflow: hidden;
+  }
+
+  // TODO: 임시로 200px 설정 (추후 자식 요소의 height에 맞게 늘어날 수 있도록 수정 필요)
+  100% {
+    display: block;
+    height: 200px;
+    border-top: 1px solid ${theme.color.gray_200};
+    padding: 10px 15px;
+    overflow: visible;
+  }
+`;
+
+const close = (theme: Theme) => keyframes`
+  0% {
+    display: block;
+    height: 200px;
+    border-top: 1px solid ${theme.color.gray_200};
+    padding: 10px 15px;
+    overflow: visible;
+  }
+
+  100% {
+    display: none;
+    height: 0;
+    border-top: 0;
+    padding: 0 15px;
+    overflow: hidden;
+  }
+`;
+
+export const SubNavList = styled.ul<{ isActive: boolean }>`
+  ${({ theme, isActive }) => css`
+    display: ${!isActive && "none"};
+    background-color: ${theme.color.gray_100};
+    transition: 0.5s;
+    animation: ${isActive ? open(theme) : close(theme)} 0.2s ease-out forwards;
+
+    & > li {
+      display: flex;
+      align-items: center;
+      height: 30px;
+    }
+  `}
+`;
+
+const parentNavStyle = (theme: Theme) => css`
+  ${theme.font.regular_14};
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 40px;
+  padding: 0 15px;
+  color: ${theme.color.black};
+`;
+
+export const Button = styled.button`
+  ${({ theme }) => css`
+    ${parentNavStyle(theme)};
+  `}
+`;
+
+export const NavLink = styled(Link)`
+  ${({ theme }) => css`
+    ${parentNavStyle(theme)};
+  `}
+`;
+
+export const SubNavLink = styled(Link)`
+  ${({ theme }) => css`
+    ${theme.font.regular_14};
+    color: ${theme.color.gray_900};
   `}
 `;
--- src/components/header/mobile/sidebar/MobileHeaderSidebar.tsx
+++ src/components/header/mobile/sidebar/MobileHeaderSidebar.tsx
@@ -1,14 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { useDevice, useOnClickOutside, useSidebar } from "@/hooks";
+import PersonIcon from "@/assets/icon/ic_person.svg";
+import CloseIcon from "@/assets/icon/ic_close.svg";
 import * as S from "./MobileHeaderSidebar.styled";
 
 const MobileHeaderSidebar = () => {
+  const [activeIndex, setActiveIndex] = useState(-1);
   const { isMobile } = useDevice();
   const { sidebarRef, sidebar, closeSidebar } = useSidebar();
   useOnClickOutside(sidebarRef, closeSidebar);
 
   const isLogin = false;
+
+  const handleParentMenuClick = (index: number) => () => {
+    setActiveIndex(index === activeIndex ? -1 : index);
+  };
 
   useEffect(() => {
     if (isMobile || !sidebar.isShow) return;
@@ -19,12 +26,81 @@ const MobileHeaderSidebar = () => {
   return (
     // TODO: 추후 수정
     <S.Sidebar ref={sidebarRef} isShow={sidebar.isShow}>
-      <div>
-        <span>프로필</span>
-        <button type="button" aria-label="Close sidebar" onClick={closeSidebar}>
-          닫기
-        </button>
-      </div>
+      <S.MenuWrapper>
+        <S.ProfileLink aria-label="User profile" href="/user">
+          <PersonIcon />
+        </S.ProfileLink>
+        <S.CloseButton
+          type="button"
+          aria-label="Close sidebar"
+          onClick={closeSidebar}
+        >
+          <CloseIcon />
+        </S.CloseButton>
+      </S.MenuWrapper>
+      <nav>
+        <ul>
+          <S.MainNavItem>
+            <S.Button type="button" onClick={handleParentMenuClick(0)}>
+              이침 교육
+            </S.Button>
+            <S.SubNavList isActive={activeIndex === 0}>
+              <li>
+                <S.SubNavLink href="/">주요 강의</S.SubNavLink>
+              </li>
+              <li>
+                <S.SubNavLink href="/">Q&A</S.SubNavLink>
+              </li>
+            </S.SubNavList>
+          </S.MainNavItem>
+          <S.MainNavItem>
+            <S.Button onClick={handleParentMenuClick(1)}>이침 소개</S.Button>
+            <S.SubNavList isActive={activeIndex === 1}>
+              <li>
+                <S.SubNavLink href="/">이침소개</S.SubNavLink>
+              </li>
+              <li>
+                <S.SubNavLink href="/">이침이란?</S.SubNavLink>
+              </li>
+              <li>
+                <S.SubNavLink href="/">이침의 유래와 역사</S.SubNavLink>
+              </li>
+              <li>
+                <S.SubNavLink href="/">이침 맛보기</S.SubNavLink>
+              </li>
+            </S.SubNavList>
+          </S.MainNavItem>
+          <S.MainNavItem>
+            <S.Button type="button" onClick={handleParentMenuClick(2)}>
+              협회 소개
+            </S.Button>
+            <S.SubNavList isActive={activeIndex === 2}>
+              <li>
+                <S.SubNavLink
+                  href="/company/about/introduction"
+                  onClick={closeSidebar}
+                >
+                  협회 소개
+                </S.SubNavLink>
+              </li>
+              <li>
+                <S.SubNavLink href="/">공지사항</S.SubNavLink>
+              </li>
+              <li>
+                <S.SubNavLink href="/">문의하기</S.SubNavLink>
+              </li>
+              <li>
+                <S.SubNavLink href="/">언론에서 본 협회</S.SubNavLink>
+              </li>
+            </S.SubNavList>
+          </S.MainNavItem>
+          <S.MainNavItem>
+            <S.NavLink href="/login" onClick={closeSidebar}>
+              자료 판매
+            </S.NavLink>
+          </S.MainNavItem>
+        </ul>
+      </nav>
     </S.Sidebar>
   );
 };
